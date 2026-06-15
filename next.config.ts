d63d26e7e8a1c248683a6074424849cd780687a5
Fuluoyu <fuluoyu@Fuluoyus-Laptop.local>
2026-06-15 23:47:04 +1000

    add static export config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/lacanian-survey",
   allowedDevOrigins: ["192.168.0.169", "*.trycloudflare.com"],
 };
 
